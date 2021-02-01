a82f8a671cf547c69aa54aeee6d651262297cc17
Michael Rios <michael.rios093@gmail.com>
2021-02-01 16:22:12 -0500

    configured :)

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 const Sequelize = require('sequelize');
 const { STRING } = Sequelize;
 
-const config = {};
-if(process.env.SSL){
-    console.log('yes');
-    config.dialectOptions = {
-        ssl : {
-            rejectUnauthorized: false
-        }
-    };
-}
+const config = {  ssl: {
+    rejectUnauthorized: false
+  }};
+// if(process.env.SSL){
+//     console.log('--------------yes-------------------');
+//     config.dialectOptions = {
+//         ssl : {
+//             rejectUnauthorized: false
+//         }
+//     };
+// }
 
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:JerryPine@localhost/acme_db', config);
 
